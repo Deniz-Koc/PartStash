3f0886ea3da54de017e336738815b68948e03226
Deniz Koc <dkocusa@outlook.com>
2026-06-15 18:33:51 -0500

    Add authentication endpoints: register, login, logout

--- partproject/urls.py
+++ partproject/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from partapi.views import register_user, login_user, logout_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
+    path("register", register_user),
+    path("login", login_user),
+    path("logout", logout_user),
 ]
-
