9b44982914d2d96576c3269a7a4b92bdae789dad
Deniz Koc <dkocusa@outlook.com>
2026-06-16 20:26:06 -0500

    Add ProjectComponent API

--- partapi/views/__init__.py
+++ partapi/views/__init__.py
@@ -2,3 +2,4 @@ from .auth import register_user, login_user, logout_user
 from .project import ProjectViewSet
 from .component import ComponentViewSet
 from .category import CategoryViewSet
+from .project_component import ProjectComponentViewSet
